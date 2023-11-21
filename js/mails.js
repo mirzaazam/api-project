5b626a3c941a9ae50d44135facf976b202a885a6
mirza-azam <mirzaazam884@gmail.com>
2023-11-21 15:14:33 +0600

    added modal when click  item details btn

--- js/mails.js
+++ js/mails.js
@@ -1,11 +1,11 @@
-
+//fetch data every item
 const getMailsData = (item) => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${item}`)
         .then(res => res.json())
         .then(data => { mainWorkHere(data); console.log(data) })
 }
 
-
+// all work here
 const mainWorkHere = data => {
     const meals = data.meals;
     const divContainer = document.getElementById('div-container');
@@ -25,7 +25,8 @@ const mainWorkHere = data => {
                           <h5 class="card-title">${item.strMeal}</h5>
                           <p class="card-text">This is good food for health</p>
                           <a href="${item.strSource}">go for details</a> <br>
-                          <a href="${item.strYoutube}">go for Video</a>
+                          <a href="${item.strYoutube}">go for Video</a> <br>
+                          <button onclick="modal(${item.idMeal})" type="button" class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Details</button>
                         </div>
                       </div>
           `;
@@ -35,7 +36,7 @@ const mainWorkHere = data => {
     })
 }
 
-//
+//search field and condition.
 const search = () => {
     const inputField = document.getElementById('input-field').value;
     console.log(inputField)
@@ -47,6 +48,25 @@ const search = () => {
     }
 }
 
+
+//add event Listner
 document.getElementById('search-btn').addEventListener('click', function click() {
     search();
 });
+
+
+//open modal when click the details btn on the card
+const modal = (idMeal) => {
+    const url = (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {setInfoModal(data.meals[0]); console.log(data.meals[0])})
+
+    const setInfoModal = (data) => {
+        document.getElementById('staticBackdropLabel').innerText = data.strMeal;
+        const modalBody = document.getElementById('modal-body');
+        modalBody.innerHTML = `
+            <img class="img-fluid" src="${data.strMealThumb}">
+        `;
+    }
+}
