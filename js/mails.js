4c850fc296583492b6201688aca3d6c135149c80
mirza-azam <mirzaazam884@gmail.com>
2023-11-21 12:26:32 +0600

    error fix

--- js/mails.js
+++ js/mails.js
@@ -2,40 +2,51 @@
 const getMailsData = (item) => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${item}`)
         .then(res => res.json())
-        .then(data => {mainWorkHere(data); console.log(data)})
+        .then(data => { mainWorkHere(data); console.log(data) })
 }
 
 
-const mainWorkHere = data =>{
+const mainWorkHere = data => {
     const meals = data.meals;
-            const divContainer = document.getElementById('div-container');
+    const divContainer = document.getElementById('div-container');
+    divContainer.innerHTML = '';
 
-            //search for each item
-            meals.forEach(item => {
-                //create a div
-                const innerDiv = document.createElement('div');
-                //add a class
-                innerDiv.classList.add('col');
-                //refresh
-                innerDiv.innerText = '';
-                //set value
-                innerDiv.innerHTML = `
+    //search for each item
+    meals.forEach(item => {
+        //create a div
+        const innerDiv = document.createElement('div');
+        //add a class
+        innerDiv.classList.add('col');
+        //set value
+        innerDiv.innerHTML = `
             <div class="card h-100">
                         <img src="${item.strMealThumb}" class="card-img-top" alt="...">
                         <div class="card-body">
                           <h5 class="card-title">${item.strMeal}</h5>
-                          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                          <p class="card-text">This is good food for health</p>
+                          <a href="${item.strSource}">go for details</a> <br>
+                          <a href="${item.strYoutube}">go for Video</a>
                         </div>
                       </div>
           `;
-                divContainer.appendChild(innerDiv);
+        //appendChild      
+        divContainer.appendChild(innerDiv);
 
-            })
-        }
-
-
-const search = () => {
-    const inputField = document.getElementById('input-field').value;
-    getMailsData(inputField);
+    })
 }
 
+//
+const search = () => {
+    const inputField = document.getElementById('input-field').value;
+    console.log(inputField)
+    if (inputField == '') {
+        alert('Please write any food!')
+    }
+    else {
+        getMailsData(inputField);
+    }
+}
+
+document.getElementById('search-btn').addEventListener('click', function click() {
+    search();
+});
